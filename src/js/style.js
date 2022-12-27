6b35f7fde71d97b5a5d4995a8b9c626a25780413
neonghostdev <neonghostdev@gmail.com>
2022-12-26 20:09:18 -0500

    📱 Mobile view: Navigation bar background styles toggle added

--- src/js/style.js
+++ src/js/style.js
@@ -2,6 +2,9 @@
 const burgerMenu = document.querySelector("#burgerIcon");
 const closeMenuIcon = document.querySelector("#closeMenuIcon");
 const mobileMenu = document.querySelector("#mobileMenu");
+const headerIconsContainer = document.querySelector(".header__icons-container");
+const main = document.querySelector(".main");
+const body = document.querySelector("body");
 
 function toggleHidden(element, elementBEMName) {
   element.classList.toggle(elementBEMName);
@@ -13,4 +16,20 @@ burgerMenu.addEventListener("click", () => {
 
 closeMenuIcon.addEventListener("click", () => {
   toggleHidden(mobileMenu, "mobile-menu--hidden");
+});
+
+main.addEventListener("click", () => {
+  mobileMenu.classList.add("mobile-menu--hidden");
+});
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY === 0) {
+    headerIconsContainer.classList.add(
+      "header__icons-container--no-background"
+    );
+  } else {
+    headerIconsContainer.classList.remove(
+      "header__icons-container--no-background"
+    );
+  }
 });
